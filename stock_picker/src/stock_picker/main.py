9bff49d55336ae22728789c948ca656b6d47e2aa
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-11-04 20:11:46 +0100

    updating stock_picker

---
+++ stock_picker/src/stock_picker/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import sys
+import warnings
+import os
+from datetime import datetime
+
+from stock_picker.crew import StockPicker
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+
+def run():
+    """
+    Run the research crew.
+    """
+
+    inputs = {"sector": "Technology", "current_date": str(datetime.now())}
+
+    result = StockPicker().crew().kickoff(inputs=inputs)
+    print(result.raw)
+
+if __name__ == "__main__":
+    run()
